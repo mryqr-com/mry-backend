35939fbf8413f5211cd02b46ecd4984efd3c0abb
Teng Yun <davenkin@126.com>
2026-05-25 20:05:18 +0800

    adde maven

---
+++ run-local.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+./start-docker-compose.sh
+
+./mvnw spring-boot:run \
+  -Dspring-boot.run.profiles=local \
+  -Dspring-boot.run.jvmArguments="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005"
